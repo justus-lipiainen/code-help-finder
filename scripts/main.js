53f38181080175c8c9fc8207a4c878892b9df473
Justus Lipiäinen <92686534+justus-lipiainen@users.noreply.github.com>
2025-11-13 10:23:46 +0200

    fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-const API_KEY = 'AIzaSyCUeTqSkHcoJTvBpTfvmtzn5UJ7pq_cbH0';
-const CSE_ID = '356bb56d9aadd499';
-
 async function search() {
     const query = document.getElementById('query').value.trim();
     if (!query) {
