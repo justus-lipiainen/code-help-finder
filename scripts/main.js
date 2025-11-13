14d0fa4ab0a99b0880a0e1676c39460a31bd521c
Justus Lipiäinen <92686534+justus-lipiainen@users.noreply.github.com>
2025-11-13 11:14:39 +0200

    fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+const API_KEY = 'AIzaSyCUeTqSkHcoJTvBpTfvmtzn5UJ7pq_cbH0';
+const CSE_ID = '356bb56d9aadd499f';
+
 async function search() {
     const query = document.getElementById('query').value.trim();
     if (!query) {
